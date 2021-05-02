9546801f3dd408a0b743e94fc0d980619ae79a13
LDami <ldami@live.fr>
2021-05-02 23:22:59 +0200

    Fixed issue where the vehicle exploded at missile launch

--- SampSharpGameMode1/Physics/CollisionManager.cs
+++ SampSharpGameMode1/Physics/CollisionManager.cs
@@ -1,5 +1,6 @@
 ﻿using SampSharp.GameMode;
 using SampSharp.GameMode.World;
+using SampSharp.Streamer;
 using SampSharp.Streamer.World;
 using System;
 using System.Collections.Generic;
@@ -10,21 +11,27 @@ namespace SampSharpGameMode1.Physics
 {
 	public class CollisionManager
 	{
-		public static void ExplodeOnCollision(DynamicObject obj, Vector3 dest)
+		/// <summary>
+		///		This method explodes a DynamicObject when he collides a wall or a vehicle
+		/// </summary>
+		/// <param name="obj">The <see cref="DynamicObject"/> to explode</param>
+		/// <param name="dest">The destination where <paramref name="obj"/> goes</param>
+		/// <param name="sender">The sender of obj. Used to prevent self-explosion at launch</param>
+		public static void ExplodeOnCollision(DynamicObject obj, Vector3 dest, BasePlayer sender)
 		{
 			Thread t = new Thread(() =>
 			{
 				RayCastCollisionTarget collisionTarget;
 				Vector3 objPos; // Contains the obj position to be set just after the IsDisposed check
 				bool collisionWithVehicle;
-				while (!obj.IsDisposed)
+				while(!obj.IsDisposed)
 				{
 					objPos = obj.Position;
 					collisionTarget = ColAndreas.RayCastLine(objPos, dest);
 					collisionWithVehicle = false;
-					foreach (BaseVehicle veh in BaseVehicle.All)
+					foreach(BaseVehicle veh in BaseVehicle.All)
 					{
-						if (veh.GetDistanceFromPoint(objPos) < 10.0) collisionWithVehicle = true;
+						if(veh.GetDistanceFromPoint(objPos) < 5.0 && !sender.Vehicle.Equals(veh)) collisionWithVehicle = true;
 					}
 					if(collisionTarget.distance < 10.0 || collisionWithVehicle)
 					{
