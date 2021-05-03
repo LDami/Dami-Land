0e34de3f3759f16a509c8cb79dea50c7650ae23b
LDami <ldami@live.fr>
2021-05-03 22:45:05 +0200

    Added color palette

---
+++ SampSharpGameMode1/Display/ColorPalette.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace SampSharpGameMode1.Display
+{
+	public class ColorPalette
+	{
+		public class SAMPColor
+		{
+			private Color color;
+			public SAMPColor(Color clr)
+			{
+				this.color = clr;
+			}
+			public override string ToString()
+			{
+				return "{" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2") + "}";
+			}
+		}
+		public static class Primary
+		{
+			public static SAMPColor Main = new SAMPColor(Color.FromArgb(76, 117, 183));
+			public static SAMPColor Lighten = new SAMPColor(Color.FromArgb(112, 145, 197));
+			public static SAMPColor Darken = new SAMPColor(Color.FromArgb(53, 82, 128));
+		}
+		public static class Secondary
+		{
+			public static SAMPColor Main = new SAMPColor(Color.FromArgb(231, 236, 239));
+			public static SAMPColor Lighten = new SAMPColor(Color.FromArgb(243, 245, 247));
+			public static SAMPColor Darken = new SAMPColor(Color.FromArgb(194, 207, 214));
+		}
+
+	}
+}
