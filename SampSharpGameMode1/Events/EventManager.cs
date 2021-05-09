c8de542d07e92ab1aed0acae544cfea059f8f384
LDami <ldami@live.fr>
2021-05-09 03:04:41 +0200

    Implemented Derby in EventManager

--- SampSharpGameMode1/Events/EventManager.cs
+++ SampSharpGameMode1/Events/EventManager.cs
@@ -3,6 +3,7 @@ using SampSharp.GameMode.Definitions;
 using SampSharp.GameMode.Display;
 using SampSharp.GameMode.SAMP;
 using SampSharpGameMode1.Events.Races;
+using SampSharpGameMode1.Events.Derbys;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -124,60 +125,75 @@ namespace SampSharpGameMode1.Events
 
         public void ShowCreateEventNameDialog(Player player, EventType eventType)
         {
-            switch(eventType)
+            InputDialog createEventNameDialog = new InputDialog(eventType.ToString() + " name", "Type the " + eventType.ToString() + " name you are looking for, or empty for random. You can also search by creator name", false, "Search", "Cancel");
+            createEventNameDialog.Show(player);
+            createEventNameDialog.Response += (sender, eventArgs) =>
             {
-                case EventType.Race:
-                    {
-                        InputDialog createEventNameDialog = new InputDialog("Race name", "Type the race name you are looking for, or empty for random. You can also search by creator name", false, "Search", "Cancel");
-                        createEventNameDialog.Show(player);
-                        createEventNameDialog.Response += (sender, eventArgs) =>
-                        {
-                            if (eventArgs.DialogButton == DialogButton.Left)
-                            {
-                                ShowCreateEventSearchDialog(player, EventType.Race, eventArgs.InputText);
-                            }
-                            else
-                            {
-                                player.Notificate("Cancelled");
-                                ShowCreateEventTypeDialog(player);
-                            }
-                        };
-                        break;
-                    }
-            }
+                if (eventArgs.DialogButton == DialogButton.Left)
+                {
+                    ShowCreateEventSearchDialog(player, eventType, eventArgs.InputText);
+                }
+                else
+                {
+                    player.Notificate("Cancelled");
+                    ShowCreateEventTypeDialog(player);
+                }
+            };
         }
 
         public void ShowCreateEventSearchDialog(Player player, EventType eventType, string str)
         {
+            string query = "";
+            string key_id = "", key_name = "", key_creator = "";
+            switch(eventType)
+			{
+                case EventType.Race:
+					{
+                        query = "SELECT race_id, race_name, race_creator FROM races WHERE race_name LIKE @name OR race_creator LIKE @name";
+                        key_id = "race_id";
+                        key_name = "race_name";
+                        key_creator = "race_creator";
+                        break;
+					}
+                case EventType.Derby:
+                    {
+                        query = "SELECT derby_id, derby_name, derby_creator FROM derbys WHERE derby_name LIKE @name OR derby_creator LIKE @name";
+                        key_id = "derby_id";
+                        key_name = "derby_name";
+                        key_creator = "derby_creator";
+                        break;
+					}
+			}
             Dictionary<string, string> row;
             Dictionary<string, object> param = new Dictionary<string, object>
                 {
                     { "@name", str }
                 };
-            GameMode.mySQLConnector.OpenReader("SELECT race_id, race_name, race_creator FROM races WHERE race_name LIKE @name OR race_creator LIKE @name", param);
+            GameMode.mySQLConnector.OpenReader(query, param);
             row = GameMode.mySQLConnector.GetNextRow();
             if(row.Count == 0)
             {
-                player.Notificate("No race found");
+                player.Notificate("No results");
                 GameMode.mySQLConnector.CloseReader();
                 ShowCreateEventNameDialog(player, eventType);
             }
             else
             {
-                List<int> foundRaces = new List<int>();
-                ListDialog eventSearchDialog = new ListDialog("Found races", "Launch", "Cancel");
+                List<int> foundEvents = new List<int>();
+                ListDialog eventSearchDialog = new ListDialog("Found " + eventType.ToString() + "s", "Launch", "Cancel");
                 while (row.Count > 0)
                 {
-                    foundRaces.Add(Convert.ToInt32(row["race_id"]));
-                    eventSearchDialog.AddItem(row["race_id"] + "_" + Display.ColorPalette.Primary.Main + row["race_name"] + Display.ColorPalette.Primary.Lighten + " by " + Display.ColorPalette.Primary.Main + row["race_creator"]);
+                    foundEvents.Add(Convert.ToInt32(row[key_id]));
+                    eventSearchDialog.AddItem(row[key_id] + "_" + Display.ColorPalette.Primary.Main + row[key_name] + Display.ColorPalette.Primary.Lighten + " by " + Display.ColorPalette.Primary.Main + row[key_creator]);
                     row = GameMode.mySQLConnector.GetNextRow();
                 }
+                GameMode.mySQLConnector.CloseReader();
                 eventSearchDialog.Show(player);
                 eventSearchDialog.Response += (sender, eventArgs) =>
                 {
                     if (eventArgs.DialogButton == DialogButton.Left)
                     {
-                        CreateEvent(player, eventType, foundRaces[eventArgs.ListItem]);
+                        CreateEvent(player, eventType, foundEvents[eventArgs.ListItem]);
                     }
                     else
                     {
@@ -185,8 +201,6 @@ namespace SampSharpGameMode1.Events
                         ShowCreateEventTypeDialog(player);
                     }
                 };
-
-                GameMode.mySQLConnector.CloseReader();
             }
         }
 
@@ -201,9 +215,9 @@ namespace SampSharpGameMode1.Events
                         {
                             if (eventArgs.ErrorMessage == null)
                             {
-                                if(player.IsConnected) player.SendClientMessage(Color.Green, "Event " + eventArgs.EventLoaded.Id + " loaded ! (Race)");
+                                if (player.IsConnected) player.SendClientMessage(Color.Green, "Event " + Display.ColorPalette.Primary.Main + eventArgs.EventLoaded.Id + Color.Green + " loaded ! (Race)");
                                 eventList.Add(eventArgs.EventLoaded);
-                                if(openedEvent == null)
+                                if (openedEvent == null)
                                 {
                                     openedEvent = eventArgs.EventLoaded;
                                     eventArgs.EventLoaded.Open();
@@ -215,6 +229,30 @@ namespace SampSharpGameMode1.Events
                                 if (player.IsConnected) player.SendClientMessage(Color.Red, "Cannot load the race: " + eventArgs.ErrorMessage);
                         };
                         player.SendClientMessage(Color.Green, "Loading Race #" + id);
+                        newEvent.Load();
+                        break;
+                    }
+                case EventType.Derby:
+                    {
+                        Event newEvent = new DerbyEvent(id);
+                        newEvent.Loaded += (sender, eventArgs) =>
+                        {
+                            if (eventArgs.ErrorMessage == null)
+                            {
+                                if (player.IsConnected) player.SendClientMessage(Color.Green, "Event " + Display.ColorPalette.Primary.Main + eventArgs.EventLoaded.Id + Color.Green + " loaded ! (Derby)");
+                                eventList.Add(eventArgs.EventLoaded);
+                                if (openedEvent == null)
+                                {
+                                    openedEvent = eventArgs.EventLoaded;
+                                    eventArgs.EventLoaded.Open();
+                                    eventArgs.EventLoaded.Started += (sender, eventArgs) => { openedEvent = null; };
+                                    eventArgs.EventLoaded.Ended += (sender, eventArgs) => { eventList.Remove((Event)sender); };
+                                }
+                            }
+                            else
+                                if (player.IsConnected) player.SendClientMessage(Color.Red, "Cannot load the derby: " + eventArgs.ErrorMessage);
+                        };
+                        player.SendClientMessage(Color.Green, "Loading Derby #" + id);
                         newEvent.Load();
                         break;
                     }
